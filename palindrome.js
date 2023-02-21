56c3e102faadb1460d2fed6313f6a2dbef2159f8
T L Mazemaker <92136680+TLMazemaker@users.noreply.github.com>
2023-02-21 20:16:45 +0800

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,17 +1,16 @@
 function isPalindrome(str) { // Creating a function to check the user input
-  let palindrome = str.str.toLowerCase().replace(/[^a-z0-9]/g, '')); //Take a user input, change it to lowercase, remove all punctuations and whitespaces, then store it in a variable.
-  if(str === palindrome.split('').reverse().join('') {
-     return True;
+  let palindrome = str.toLowerCase().replace(/[^a-z0-9]/g, ''); //Take a user input, change it to lowercase, remove all punctuations and whitespaces, then store it in a variable.
+  if(str === palindrome.split('').reverse().join('')) {
+     return true;
      }
   else{
-    return False;
+    return false;
   }
 }
 
-let str = prompt("Input your phrase");
-if(isPalindrome(str)){
-  console.log(str+" is a palindrome.");
+if(isPalindrome('')) { //Enter your text here
+  console.log('This is a palindrome');
 }
 else{
-  console.log(str+" is not a palindrome.");
+  console.log("This is not a palindrome");
 }
